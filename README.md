d33aee21042e9ceac0669167b3eaafa867b6864c
corentinleberre <corentin.leb29@gmail.com>
2022-11-21 22:09:06 -0500

    :sparkles: Retrieving the availability of bikes attached to a Bixi station

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# bixi-around-go
+# Bixi-around-go
+
+Same project as [here](https://github.com/corentinleberre/bixi-around), but written in Go.
---
+++ app.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	router := gin.Default()
+	router.Use(gin.Recovery())
+	router.GET("/api/bixi-around", func(c *gin.Context) {
+		city := c.DefaultQuery("city", "montreal")
+		nbResult, _ := strconv.ParseInt(c.DefaultQuery("nbResult", "5"), 0, 16)
+		lat, _ := strconv.ParseFloat(c.DefaultQuery("lat", "45.501690"), 64)
+		lon, _ := strconv.ParseFloat(c.DefaultQuery("lon", "-73.567253"), 64)
+		c.JSON(http.StatusOK, bixiLogic(city, int(nbResult), lat, lon))
+	})
+	router.Run(":3000")
+}
